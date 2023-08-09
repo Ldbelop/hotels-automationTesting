8a177ce051b7e71ef314a1e83f8bb7eceab2582e
David Barón <baronavilandavid@gmail.com>
2023-08-09 17:46:12 -0500

    Se hace el primer test | Todas las tarjetas cargan

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,5 +1,14 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  let requestedCardsNumber;
+  beforeEach(() => {
+    cy.viewport(1920, 1080)
+    cy.visit('https://ldbelop.github.io/hotels/')
+  })
+  it('All the cards loaded correctly', () => {
+    cy.request('GET', 'https://6256097e8646add390e01d99.mockapi.io/hotels/reservation/hotels').then((response) =>{
+      requestedCardsNumber = response.body.length;
+      expect(response.body.length).to.be.greaterThan(0)
+      cy.get(".hotelCard").should("have.length",requestedCardsNumber)
+    })
   })
 })
